10279afb585cb2cf026976b40640b85016d6b25e
Toan Nguyen <toan.nguyen@smartdev.com>
2022-10-25 11:27:15 +0700

    bai 48 global style trong styled component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import styled from "styled-components";
 
 import Card from "./components/card2/Card";
 import CardList from "./components/card2/CardList";
+import { GlobalStyles } from "./GlobalStyles";
 
 const App = () => {
   return (
     <div style={{ padding: "10px", backgroundColor: "#eee", height: "1000px" }}>
+      <GlobalStyles></GlobalStyles>
       <CardList>
         <Card secondary />
         <Card />
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.scss";
+// import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
