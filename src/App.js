cba472264e535008fab815acf7f7e74bd4e4d5ca
Toan Nguyen <toan.nguyen@smartdev.com>
2022-10-25 23:09:59 +0700

    bai 65 refactor code sang async await

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
+import "./index.scss";
 
-import Card from "./components/card2/Card";
-import Card2 from "./components/card2/Card2";
-import CardList from "./components/card2/CardList";
-import { GlobalStyles } from "./GlobalStyles";
+import Photos2 from "./components/photo/Photos2";
 
 const App = () => {
   return (
     <div style={{ padding: "10px", backgroundColor: "#eee", height: "1000px" }}>
-      <GlobalStyles></GlobalStyles>
-      <CardList>
-        <Card2 />
-        <Card2 />
-        <Card2 />
-      </CardList>
+      <Photos2 />
     </div>
   );
 };
