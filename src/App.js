1e1186ae9692655b67e3ff1ee25467f0887c1839
toan_nguyen <ndtoan@enjoyworks.co.kr>
2023-02-23 15:19:33 +0700

    127 validate formik

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import SignUpForm from "./components/form/SignUpForm";
+import SignUpFormV2 from "./components/form/SignUpFormV2";
 
 import "./index.scss";
 
 const App = () => {
-  return <div className="p-5">app ne</div>;
+  return (
+    <div className="p-5">
+      {/* <SignUpForm /> */}
+      <SignUpFormV2 />
+    </div>
+  );
 };
 
 export default App;
