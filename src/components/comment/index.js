e8c92d8867073e6a3e3ca153fa8c989f26a00371
toan_nguyen <ndtoan@enjoyworks.co.kr>
2023-03-10 17:12:05 +0700

    add comment

---
+++ src/components/comment/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./styles.scss";
+
+const Comment = () => {
+  return <h2>comment</h2>;
+};
+
+export default Comment;
+
+const comments = [{}];
