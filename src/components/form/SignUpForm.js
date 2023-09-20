fa643c61ebd13111d36f079af4e973d0f1cef0a0
toan_nguyen <ndtoan@enjoyworks.co.kr>
2023-09-20 15:59:47 +0700

    124 validation co ban voi formik

--- src/components/form/SignUpForm.js
+++ src/components/form/SignUpForm.js
@@ -1,10 +1,26 @@
 import { useFormik } from "formik";
 
 const SignUpForm = () => {
+  const validate = (values) => {
+    const errors = {};
+    if (!values.firstName) {
+      errors.firstName = "Please enter this field";
+    } else if (values.firstName?.length > 10) {
+      errors.firstName = "Please enter less than 10 characters";
+    }
+    if (!values.lastName) {
+      errors.lastName = "Please enter this field";
+    } else if (values.lastName?.length > 10) {
+      errors.lastName = "Please enter less than 10 characters";
+    }
+    return errors;
+  };
   const formik = useFormik({
     initialValues: {
       firstName: "",
+      lastName: "",
     },
+    validate,
     onSubmit: (values) => {
       console.log("values", values);
     },
@@ -14,6 +30,7 @@ const SignUpForm = () => {
     <form
       onSubmit={formik.handleSubmit}
       className="p-10 w-full max-w-[500px] mx-auto"
+      autoComplete="off"
     >
       <div className="flex flex-col gap-2 mb-5">
         <label htmlFor="firstName">First Name</label>
@@ -26,6 +43,25 @@ const SignUpForm = () => {
           value={formik.values.firstName}
           onChange={formik.handleChange}
         />
+        {formik.errors.firstName ? (
+          <p className="text-red-500 text-sm">{formik.errors.firstName}</p>
+        ) : null}
+      </div>
+
+      <div className="flex flex-col gap-2 mb-5">
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          placeholder="Enter your last name"
+          className="p-4 rounded-md border border-gray-200"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+        />
+        {formik.errors.lastName ? (
+          <p className="text-red-500 text-sm">{formik.errors.lastName}</p>
+        ) : null}
       </div>
       <div>
         <button
